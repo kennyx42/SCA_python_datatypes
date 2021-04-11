646bb546d498fd069a35713c42e47df300137f13
kennyx42 <owosenkehinde01@gmail.com>
2021-04-11 10:05:06 +0100

    adding number exercise

---
+++ number_exe.py
@@ -0,0 +1,11 @@
+#2-8. Number Eight
+print(3.2+4.8) #addition
+print(15-7) #Subtraction
+print(8*1) #multiplication
+print(48/6) #division
+
+
+#2-9. Favourite Number
+favourite_number=5
+message=f'The favourite number is {favourite_number}'
+print(message)
