c087a9b609e8ec8032134e04d190eeff9042e340
kennyx42 <owosenkehinde01@gmail.com>
2021-04-11 09:18:47 +0100

    adding string exercise

---
+++ string_exe.py
@@ -0,0 +1,28 @@
+#2-3. Personal Message
+name= 'Kenny'
+greetings=f'Hell0 {name}, have you eaten this morning?\n'
+print(greetings)
+
+#2-4. Name Case
+name='Glorious thunDerStorm'
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+#2-5. Famous Quote
+the_night='By Avicii, "One day you will leave this world behind\n so live a life you\'ll remember\n my father told me when i was just a child\n these are the nights that never die."\n'
+print(the_night)
+
+#2-6. Famous Quote 2:
+famous_person='Avicii'
+message='"One day you will leave this world behind\n so live a life you\'ll remember\n my father told me when i was just a child\n these are the nights that never die."\n'
+print(f'By {famous_person}, {message}')
+
+#2-6. Stripping Names
+name='\talayo\n'
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+
