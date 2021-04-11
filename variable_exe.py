fa37665b3781dc8f0cc5f5230e5eb82699e532ef
kennyx42 <owosenkehinde01@gmail.com>
2021-04-11 07:58:37 +0100

    adding Variable questions

---
+++ variable_exe.py
@@ -0,0 +1,8 @@
+question_1= 'This is the first question'
+print('1.',' ',question_1)
+
+question_2a='This is the second question'
+print('2a.',' ',question_2a)
+
+question_2a='Changing the value of variable question_2a to a new message '
+print('2b.',' ',question_2a)
